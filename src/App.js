125a0bfb56827a9d24ab97e136cd84fa3907c361
BEDO <siegen567@gmail.com>
2023-06-16 23:06:19 +0300

    sss
    ssss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import Cart from './components/Cart/Cart';
-import CartProvider from './store/CartProvider';
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
+import classes from "./App.module.css"; // Import the CSS module
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -16,13 +17,19 @@ function App() {
     setCartIsShown(false);
   };
 
+  //i add below
+
+  ///////
+
   return (
     <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler} />}
-      <Header onShowCart={showCartHandler} />
-      <main>
-        <Meals />
-      </main>
+      <div className={classes.App}>
+        {cartIsShown && <Cart onClose={hideCartHandler} />}
+        <Header onShowCart={showCartHandler} />
+        <main>
+          <Meals />
+        </main>
+      </div>
     </CartProvider>
   );
 }
